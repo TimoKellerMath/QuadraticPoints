d62ddae9b308e4449fc96f75089cb8f33c569db1
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-03-06 13:10:14 +0000

    correcting typo

--- AL_sieve.m
+++ AL_sieve.m
@@ -15,7 +15,7 @@ for N in [74, 85, 86, 97, 103, 109, 121, 127] do
 
     // Include any additional points.
     
-    if N eq 74 thentup3
+    if N eq 74 then
         K7<r7> := QuadraticField(-7);
         P1 := <K7, [-1, 0, 0, 1/r7, 2/r7, -2/r7, 1/r7, 1]>;
         P2 := <K7, [-1, 0, 0, -1/r7, 2/r7, -2/r7, 1/r7, -1]>;
